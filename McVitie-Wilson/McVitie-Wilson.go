8336bea9da526470a1484c363839a9c3785d4fe5
Kblac0 <kblac098@uottawa.ca>
2026-02-26 17:33:26 -0500

    Skeleton

--- McVitie-Wilson/McVitie-Wilson.go
+++ McVitie-Wilson/McVitie-Wilson.go
@@ -2,27 +2,76 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sync"
 	"time"
 )
 
+type Resident struct {
+	residentID     int
+	firstname      string
+	lastname       string
+	rol            []string
+	matchedProgram string
+}
+
+type Program struct {
+	programID         string
+	name              string
+	nPositions        int
+	rol               []int
+	selectedResidents []int
+}
+
 func offer() {
-	fmt.Printf("offer_single")
+	return
 }
 
 func evaluate() {
-	fmt.Printf("evaluate_single-thread")
+	return
 }
 
-func McVitie_Wilson_Single_Thread() {
+func loadResidents(path string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("Error: %s could not be loaded. %s\n", path, err)
+		return
+	}
+	defer file.Close()
+
+	fmt.Printf("\n%s loaded successfully\n", path)
+}
+
+func loadPrograms(path string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("\n%s loaded successfully\n", path)
+}
+
+func McVitie_Wilson() {
 	offer()
 	evaluate()
 }
 
 func main() {
+
+	var (
+		resident_path string = "residentSmall.csv"
+		program_path  string = "programSmall.csv"
+	)
+
 	start := time.Now()
 
 	var wg sync.WaitGroup
+	wg.Add(2)
 
-	McVitie_Wilson_Single_Thread()
+	go loadResidents(resident_path, &wg)
+	go loadPrograms(program_path, &wg)
+	wg.Wait()
+
+	McVitie_Wilson()
+
+	end := time.Now()
+	fmt.Printf("\nExecution time: %s\n", end.Sub(start))
 }
